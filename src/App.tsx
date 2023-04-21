3959111f35e72b4061da6196f08b19be6099a2b2
Saimon <saimon22011@gmail.com>
2023-04-21 19:38:35 +0300

    del: GitHub banner + upd: main -> post_block

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { RefineSnackbarProvider, notificationProvider } from "@refinedev/mui";
 
 import { CssBaseline, GlobalStyles } from "@mui/material";
 import routerBindings, {
-  UnsavedChangesNotifier,
+    NavigateToResource,
+    UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MuiInferencer } from "@refinedev/inferencer/mui";
+
 import { ColorModeContextProvider } from "./contexts/color-mode";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <CssBaseline />
@@ -24,13 +26,43 @@ function App() {
               notificationProvider={notificationProvider}
               routerProvider={routerBindings}
               dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+              resources={[
+                  {
+                      name: "blog_posts",
+                      list: "/blog-posts",
+                      show: "/blog-posts/show/:id",
+                      create: "/blog-posts/create",
+                      edit: "/blog-posts/edit/:id",
+                  },
+              ]}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                  <Route
+                      index
+                      element={
+                          <NavigateToResource resource="blog_posts" />
+                      }
+                  />
+                  <Route path="blog-posts">
+                      <Route index element={<MuiInferencer />} />
+                      <Route
+                          path="show/:id"
+                          element={<MuiInferencer />}
+                      />
+                      <Route
+                          path="edit/:id"
+                          element={<MuiInferencer />}
+                      />
+                      <Route
+                          path="create"
+                          element={<MuiInferencer />}
+                      />
+                  </Route>
+
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
